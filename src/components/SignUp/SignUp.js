dd5398f2aa4805e684ea4f6b0fdb46ceb984b74e
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-17 17:56:17 +0600

    update

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/UserContext";
 import "./SignUp.css";
 
 const SignUp = () => {
   const [error, setError] = useState(null);
+  const { createUser } = useContext(AuthContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,9 +25,14 @@ const SignUp = () => {
     }
 
     setError(null);
-    console.log(email, password, confirm);
 
-    form.reset();
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+      })
+      .catch((error) => console.error(error));
   };
   return (
     <div className="form-container">
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -8,7 +8,11 @@ const auth = getAuth(app);
 const UserContext = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  const authInfo = { user };
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = { user, createUser };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
