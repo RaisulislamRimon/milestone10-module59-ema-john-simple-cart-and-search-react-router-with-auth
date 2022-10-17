80fcfc40b18e0d73dbf84f4a189df9725b866d68
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-17 18:08:44 +0600

    update SignIn/Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/UserContext";
 import "./Login.css";
 
 const Login = () => {
+  const [error, setError] = useState(null);
+  const { signIn } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setError(null);
+        form.reset();
+      })
+      .catch((error) => {
+        console.error(error);
+        setError("User not found/Wrong password");
+      });
+  };
+
   return (
     <div className="form-container">
       <h2 className="form-title">Login</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" required />
@@ -15,6 +38,7 @@ const Login = () => {
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" required />
         </div>
+        {error && <p className="error-text">{error}</p>}
         <input className="btn-submit" type="submit" value="Login" />
       </form>
       <p>
