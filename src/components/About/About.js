62a3fc50cde793a7b2dc9518ceff2a9d5cad1ebc
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-17 14:25:44 +0600

    showing user email from AuthContext/UserContext in About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../contexts/UserContext";
 
 const About = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div>
       <h1>This is About</h1>
+      <p>{user?.email}</p>
     </div>
   );
 };
