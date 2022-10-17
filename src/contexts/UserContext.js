13be9a49388ec4a1b572ef94b34bbda637122ef7
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-17 17:30:25 +0600

    Get Form Data and display form error

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,9 +1,13 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
-  const user = { email: "abc" };
+  const [user, setUser] = useState(null);
+
   const authInfo = { user };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
