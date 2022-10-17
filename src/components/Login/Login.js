366af3133aa32ab14ccb217aad338bf9e5485ff6
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-17 10:37:58 +0600

    Login with css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
@@ -8,9 +9,17 @@ const Login = () => {
       <form>
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" required />
         </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
       </form>
+      <p>
+        New to ema john <Link to="/signup">Create a New Account</Link>
+      </p>
     </div>
   );
 };
